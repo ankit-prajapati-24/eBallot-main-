d54f7dd16c63f011990119fe21e0ef18a8c2f958
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2023-11-19 00:10:47 +0530

    Update Auth.js

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const OTP = require('../models/otp');
+const OTP = require('../models/OTP');
 const otpgenerator = require("otp-generator");
 const bcrypt = require("bcrypt");
 const Profile = require("../models/Profile");
